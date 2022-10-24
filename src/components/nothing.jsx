65fa9d340aa6359b4ca45c259e3f271ac80960de
B1gChungus <88466620+B1gChungus@users.noreply.github.com>
2022-10-24 14:25:27 -0700

    Delete src directory

--- src/components/nothing.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Nothing({ refresh }) {
-    return (
-        <button id="nothing" onClick={() => { refresh() }}>Refresh List</button>
-    )
-}
-
-export default Nothing
